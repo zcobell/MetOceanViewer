6cca2df79153d79342c06d55d96f1096894669f5
zcobell <zcobell@98677110-50cb-49a8-a4c7-c1441d9d56a0>
2015-01-13 17:40:46 +0000

    Changing version information

--- trunk/version.h
+++ trunk/version.h
@@ -35,21 +35,21 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION             0,9,0,0
-#define VER_FILEVERSION_STR         "0.9.0.0\0"
+#define VER_FILEVERSION             1,0,0,0
+#define VER_FILEVERSION_STR         "1.0.0.0\0"
 
-#define VER_PRODUCTVERSION          0,9,0,0
-#define VER_PRODUCTVERSION_STR      "0.9\0"
+#define VER_PRODUCTVERSION          1,0,0,0
+#define VER_PRODUCTVERSION_STR      "1.0\0"
 
-#define VER_COMPANYNAME_STR         "ARCADIS"
+#define VER_COMPANYNAME_STR         "ZachCobell"
 #define VER_FILEDESCRIPTION_STR     "ADCIRC Validator"
 #define VER_INTERNALNAME_STR        "ADCIRC_Validator"
-#define VER_LEGALCOPYRIGHT_STR      "Copyright 2014 ARCADIS"
+#define VER_LEGALCOPYRIGHT_STR      "Copyright 2015 Zach Cobell"
 #define VER_LEGALTRADEMARKS1_STR    "All Rights Reserved"
 #define VER_LEGALTRADEMARKS2_STR    VER_LEGALTRADEMARKS1_STR
 #define VER_ORIGINALFILENAME_STR    "ADCValidator.exe"
 #define VER_PRODUCTNAME_STR         "ADCValidator"
 
-#define VER_COMPANYDOMAIN_STR       "http://www.arcadis-us.com"
+#define VER_COMPANYDOMAIN_STR       "http://www.zachcobell.com"
 
 #endif // VERSION_H
