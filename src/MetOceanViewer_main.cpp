a3b5bdd3e1646b5edc7a6533f71e6220e70f476c
zcobell <zcobell@98677110-50cb-49a8-a4c7-c1441d9d56a0>
2015-07-04 16:15:18 +0000

    Adding GPL changes and logos.

--- src/MetOceanViewer_main.cpp
+++ src/MetOceanViewer_main.cpp
@@ -1,36 +1,19 @@
-//---- LGPL --------------------------------------------------------------------
+//----GPL-----------------------------------------------------------------------
 //
-// Copyright (C)  ARCADIS, 2011-2015.
+// This file is part of MetOceanViewer.
 //
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation version 3.
+//    MetOceanViewer is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
 //
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
+//    MetOceanViewer is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
 //
-// You should have received a copy of the GNU Lesser General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-// contact: Zachary Cobell, zachary.cobell@arcadis-us.com
-// ARCADIS
-// 4999 Pearl East Circle, Suite 200
-// Boulder, CO 80301
-//
-// All indications and logos of, and references to, "ARCADIS" 
-// are registered trademarks of ARCADIS, and remain the property of
-// ARCADIS. All rights reserved.
-//
-//------------------------------------------------------------------------------
-// $Author$
-// $Date$
-// $Rev$
-// $HeadURL$
-//------------------------------------------------------------------------------
-//  File: MetOceanViewer_main.cpp
-//
+//    You should have received a copy of the GNU General Public License
+//    along with MetOceanViewer.  If not, see <http://www.gnu.org/licenses/>.
 //------------------------------------------------------------------------------
         
 #include <MetOceanViewer.h>
--- src/about_dialog.cpp
+++ src/about_dialog.cpp
@@ -1,36 +1,19 @@
-//---- LGPL --------------------------------------------------------------------
+//----GPL-----------------------------------------------------------------------
 //
-// Copyright (C)  ARCADIS, 2011-2015.
+// This file is part of MetOceanViewer.
 //
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation version 3.
+//    MetOceanViewer is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
 //
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
+//    MetOceanViewer is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
 //
-// You should have received a copy of the GNU Lesser General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-// contact: Zachary Cobell, zachary.cobell@arcadis-us.com
-// ARCADIS
-// 4999 Pearl East Circle, Suite 200
-// Boulder, CO 80301
-//
-// All indications and logos of, and references to, "ARCADIS"
-// are registered trademarks of ARCADIS, and remain the property of
-// ARCADIS. All rights reserved.
-//
-//------------------------------------------------------------------------------
-// $Author$
-// $Date$
-// $Rev$
-// $HeadURL$
-//------------------------------------------------------------------------------
-//  File: about_dialog.cpp
-//
+//    You should have received a copy of the GNU General Public License
+//    along with MetOceanViewer.  If not, see <http://www.gnu.org/licenses/>.
 //------------------------------------------------------------------------------
 
 #include "about_dialog.h"
@@ -63,9 +46,9 @@ QString about_dialog::generateAboutText()
     QString text;
 
     text = "<html><body>" +
-            QString(0xA9) + " Copyright 2015 Zach Cobell, ARCADIS <br><br>" +
+            QString("<img src=&#34;img/icon.png&#34;>") +
+            QString("<br>Copyleft 2015 Zach Cobell <br><br>") +
             "<b>Version: </b> "+QString(VER_FILEVERSION_STR) + "<br><br>" +
-            "<b>Subversion Revision: </b>"+QString(SVER) + "<br><br>" +
             "<b>External Libraries</b> <br><br>" +
             "NOAA CO-OPS API - http://tidesandcurrents.noaa.gov<br>" +
             "USGS Waterdata API - http://waterdata.usgs.gov<br>" +
