4796aaeaa8947f458b757636a2f6ed8e8645bcd0
Zach Cobell <zcobell@gmail.com>
2015-07-14 09:47:25 -0600

    Fixing licensing information
    Fixing licensing information. Adding caveat that a derivative work may
    not be called "MetOcean Viewer"

--- src/hwm_data.cpp
+++ src/hwm_data.cpp
@@ -1,20 +1,25 @@
-//----GPL-----------------------------------------------------------------------
+//-------------------------------GPL-------------------------------------//
 //
-// This file is part of MetOceanViewer.
+// MetOcean Viewer - A simple interface for viewing hydrodynamic model data
+// Copyright (C) 2015  Zach Cobell
 //
-//    MetOceanViewer is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, either version 3 of the License, or
-//    (at your option) any later version.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
-//    MetOceanViewer is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 //
-//    You should have received a copy of the GNU General Public License
-//    along with MetOceanViewer.  If not, see <http://www.gnu.org/licenses/>.
-//------------------------------------------------------------------------------
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// The name "MetOcean Viewer" is specific to this project and may not be
+// used for projects "forked" or derived from this work.
+//
+//-----------------------------------------------------------------------//
         
 #include <hwm.h>
 
