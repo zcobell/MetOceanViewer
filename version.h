26246f47a9a327f1d2579f3526bba6a025f0595a
Zach Cobell <zcobell@gmail.com>
2015-07-25 16:40:42 -0600

    Removing beta tag in version.h

--- version.h
+++ version.h
@@ -24,9 +24,9 @@
 #define VERSION_H
 
 #define VER_FILEVERSION             2,1,0
-#define VER_FILEVERSION_STR         "2.1.0-beta.3"
+#define VER_FILEVERSION_STR         "2.1.0"
 
 #define VER_PRODUCTVERSION          2,1,0
-#define VER_PRODUCTVERSION_STR      "2.1.0-beta.3"
+#define VER_PRODUCTVERSION_STR      "2.1.0"
 
 #endif // VERSION_H
