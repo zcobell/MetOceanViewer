fc2c26e56a80d5ddf1bd9fb6dc2f5e964cc00f3e
zcobell <zachary.cobell@arcadis-us.com>
2016-01-31 02:07:54 -0700

    Furthering USGS class development
    Adding set/get routines and setting all variables to private.
    Also, very little now happens outside of the class by passing
    pointers into the class to the GUI elements.

--- src/ui_usgs_tab.cpp
+++ src/ui_usgs_tab.cpp
@@ -41,84 +41,20 @@ void MainWindow::on_combo_usgs_panto_currentIndexChanged(int index)
 //-------------------------------------------//
 void MainWindow::on_button_usgs_fetch_clicked()
 {
-    int i,ierr;
+    int ierr;
+
+    QApplication::setOverrideCursor(Qt::WaitCursor);
 
     //...Create a new USGS object
     if(!thisUSGS.isNull())
         delete thisUSGS;
-    thisUSGS = new usgs(this);
+    thisUSGS = new usgs(ui->usgs_map,ui->usgs_graphics,
+                        ui->radio_usgsDaily,ui->radio_usgshistoric,
+                        ui->radio_usgs_instant,ui->combo_USGSProduct,
+                        ui->Date_usgsStart,ui->Date_usgsEnd,ui->statusBar,this);
 
-    //...Check the data type
-    if(ui->radio_usgs_instant->isChecked())
-        thisUSGS->USGSdataMethod = 1;
-    else if(ui->radio_usgsDaily->isChecked())
-        thisUSGS->USGSdataMethod = 2;
-    else
-        thisUSGS->USGSdataMethod = 0;
-
-    //...Display the wait cursor
-    QApplication::setOverrideCursor(Qt::WaitCursor);
-    ui->statusBar->showMessage("Downloading data from USGS...");
-
-    //...Wipe out the combo box
-    ui->combo_USGSProduct->clear();
-
-    //...Retrieve info from google maps
-    QVariant eval = QVariant();
-    ui->usgs_map->page()->runJavaScript("returnStationID()",[&eval](const QVariant &v){eval = v;});
-    while(eval.isNull())
-        delayM(5);
-    QStringList evalList = eval.toString().split(";");
-    thisUSGS->USGSMarkerString = evalList.value(0).mid(4);
-
-    //...Sanity Check
-    if(evalList.value(0)=="none")
-    {
-        QMessageBox::warning(this,"Warning","No station has been selected.");
-        ui->statusBar->clearMessage();
-        QApplication::restoreOverrideCursor();
-        return;
-    }
-
-    //...Store the station information
-    thisUSGS->USGSMarkerID = thisUSGS->USGSMarkerString;
-    thisUSGS->CurrentUSGSStationName = evalList.value(1).simplified();
-    thisUSGS->CurrentUSGSLon = evalList.value(2).toDouble();
-    thisUSGS->CurrentUSGSLat = evalList.value(3).toDouble();
-
-    //...Get the time period for the data
-    thisUSGS->requestEndDate = ui->Date_usgsEnd->dateTime();
-    thisUSGS->requestStartDate = ui->Date_usgsStart->dateTime();
-
-    //...Grab the data from the server
-    ierr = thisUSGS->fetchUSGSData();
-    if(ierr!=0)
-    {
-        QApplication::restoreOverrideCursor();
-        ui->statusBar->clearMessage();
-        QMessageBox::critical(this,"ERROR",thisUSGS->USGSErrorString);
-        return;
-    }
-
-    //...Update the combo box
-    for(i=0;i<thisUSGS->Parameters.length();i++)
-        ui->combo_USGSProduct->addItem(thisUSGS->Parameters[i]);
-    ui->combo_USGSProduct->setCurrentIndex(0);
-    thisUSGS->ProductName = ui->combo_USGSProduct->currentText();
-
-    //...Plot the first series
-    ierr = thisUSGS->plotUSGS(ui->usgs_graphics);
-    if(ierr!=0)
-    {
-        QApplication::restoreOverrideCursor();
-        ui->statusBar->clearMessage();
-        QMessageBox::critical(this,"ERROR","No data available for this selection.");
-        return;
-    }
-
-    //...Restore the mouse pointer
+    ierr = thisUSGS->plotNewUSGSStation();
     QApplication::restoreOverrideCursor();
-    ui->statusBar->clearMessage();
 
     return;
 }
@@ -132,7 +68,7 @@ void MainWindow::on_button_usgs_fetch_clicked()
 //-------------------------------------------//
 void MainWindow::on_radio_usgs_instant_clicked()
 {
-    thisUSGS->USGSBeenPlotted = false;
+    thisUSGS->setUSGSBeenPlotted(false);
     ui->Date_usgsStart->setMinimumDateTime(QDateTime::currentDateTime().addDays(-120));
     ui->Date_usgsEnd->setMinimumDateTime(QDateTime::currentDateTime().addDays(-120));
 
@@ -153,7 +89,7 @@ void MainWindow::on_radio_usgs_instant_clicked()
 //-------------------------------------------//
 void MainWindow::on_radio_usgsDaily_clicked()
 {
-    thisUSGS->USGSBeenPlotted = false;
+    thisUSGS->setUSGSBeenPlotted(false);
     ui->Date_usgsStart->setMinimumDateTime(QDateTime(QDate(1900,1,1)));
     ui->Date_usgsEnd->setMinimumDateTime(QDateTime(QDate(1900,1,1)));
     return;
@@ -167,7 +103,7 @@ void MainWindow::on_radio_usgsDaily_clicked()
 //-------------------------------------------//
 void MainWindow::on_radio_usgshistoric_clicked()
 {
-    thisUSGS->USGSBeenPlotted = false;
+    thisUSGS->setUSGSBeenPlotted(false);
     ui->Date_usgsStart->setMinimumDateTime(QDateTime(QDate(1900,1,1)));
     ui->Date_usgsEnd->setMinimumDateTime(QDateTime(QDate(1900,1,1)));
     return;
@@ -181,15 +117,12 @@ void MainWindow::on_radio_usgshistoric_clicked()
 //-------------------------------------------//
 void MainWindow::on_combo_USGSProduct_currentIndexChanged(int index)
 {
-    QString javascript;
-    thisUSGS->USGSBeenPlotted = false;
-    if(thisUSGS->USGSDataReady)
-    {
-        thisUSGS->ProductIndex = index;
-        thisUSGS->ProductName = ui->combo_USGSProduct->currentText();
-        thisUSGS->plotUSGS(ui->usgs_graphics);
-        ui->usgs_map->page()->runJavaScript(javascript);
-    }
+    int ierr;
+    QApplication::setOverrideCursor(Qt::WaitCursor);
+    ierr = thisUSGS->replotCurrentUSGSStation(index);
+    QApplication::restoreOverrideCursor();
+    if(ierr!=0)
+        QMessageBox::critical(this,"ERROR",thisUSGS->getUSGSErrorString());
     return;
 }
 //-------------------------------------------//
