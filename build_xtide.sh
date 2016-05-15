45b39f31b42f8278d3073805c7d4ec14fcbf43de
zcobell <zcobell@gmail.com>
2016-05-15 16:30:36 -0700

    Adding copy for harmonics file

--- build_xtide.sh
+++ build_xtide.sh
@@ -19,3 +19,7 @@ cd ../xtide-2.15.1
 ./configure --prefix=$libdir CFLAGS="-I$libdir/include" CXXFLAGS="-I$libdir/include" LDFLAGS="-L$libdir/lib" --without-x
 make
 make install
+cd ../../
+
+#...Step three: Copy the harmonics file
+cp thirdparty/xtide-2.15.1/harmonics.tcd mov_libs/bin/. 
