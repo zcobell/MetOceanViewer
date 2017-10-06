9368d6ec5eb44ee3aa8c92d08e31cf2bdd6c9b0c
Zach Cobell <zachary.cobell@arcadis-us.com>
2017-10-06 13:49:05 -0600

    Removing leftover debugging statement

--- MetOceanViewer_GUI/src/movKeyhandler.cpp
+++ MetOceanViewer_GUI/src/movKeyhandler.cpp
@@ -19,26 +19,18 @@
 //-----------------------------------------------------------------------*/
 #include "movKeyhandler.h"
 #include <QKeyEvent>
-#include <QDebug>
 
 bool MovKeyhandler::eventFilter(QObject* obj, QEvent* event)
 {
     if (event->type()==QEvent::KeyPress) {
         QKeyEvent* key = static_cast<QKeyEvent*>(event);
         if ( (key->key()==Qt::Key_Enter) || (key->key()==Qt::Key_Return) )
-        {
-            qDebug() << "PRESSED!";
             emit enterKeyPressed();
-        }
         else
-        {
             return QObject::eventFilter(obj, event);
-        }
         return true;
     }
     else
-    {
         return QObject::eventFilter(obj, event);
-    }
     return false;
 }
