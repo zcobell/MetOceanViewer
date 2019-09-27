631f54629935c8d938567ffd6dc9aa57665647a4
Zach Cobell <zcobell@gmail.com>
2019-09-26 19:35:46 -0600

    Updating OSX installer script

--- MetOceanInstaller/buildDmg_MacOSX.sh
+++ MetOceanInstaller/buildDmg_MacOSX.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-QtVersion=5_13_0
+QtVersion=5_13_1
 version=$(git describe --always --tags)
-deploy="/Users/zcobell/Qt/5.13.0/clang_64/bin/macdeployqt"
+deploy="/Users/zcobell/Qt/5.13.1/clang_64/bin/macdeployqt"
 appPath="../../build-MetOcean-Desktop_Qt_"$QtVersion"_clang_64bit-Release/MetOceanViewer"
 
 $deploy $appPath/MetOceanViewer.app -dmg -qmldir=../MetOceanViewer/qml -always-overwrite
